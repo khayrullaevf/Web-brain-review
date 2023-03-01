f78144e67509630d3235e7451283a0fe2036fc9b
fazliddin3779 <fazliddinkhayrullaev4@gmail.com>
2023-03-01 10:41:22 +0500

    Review

---
+++ JavaScript/Lesson-6/while/index.js
@@ -0,0 +1,19 @@
+
+
+
+//  let me='FazliddinKhayrullaev';
+
+//  for (let i = 0; i < str.length; i++) {
+//    console.log(str[i]);
+    
+//  }
+
+
+
+ for (let i = 1; i <10; i++) {
+    console.log('I love you');
+    
+ }
+
+
+
