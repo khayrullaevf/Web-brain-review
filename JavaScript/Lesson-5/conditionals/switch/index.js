6b1dc826872e6da800e8d7895a66bc86627d0526
fazliddin3779 <fazliddinkhayrullaev4@gmail.com>
2023-03-01 09:39:15 +0500

    Switch

---
+++ JavaScript/Lesson-5/conditionals/switch/index.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+let day=4;
+
+
+switch (day) {
+    case 1: console.log('first');
+        
+        break;
+        case 2: console.log('second');
+        
+        break;  
+        
+        case 3: console.log('third');
+        
+        break; 
+        
+        case 4: console.log('forth');
+        
+        break; 
+        
+        case 5: console.log('Fifth');
+        
+        break;
+    default: console.log('not  found');
+        break;
+}
+
+
