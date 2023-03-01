9e3a0bc14be243eabaa4bdf1208a1c683891f4d5
fazliddin3779 <fazliddinkhayrullaev4@gmail.com>
2023-03-01 11:43:36 +0500

    Finding Uppercase or Lowercase

--- JavaScript/Codewars/ReveseString/index.js
+++ JavaScript/Codewars/ReveseString/index.js
@@ -9,18 +9,41 @@
 
 
 
-function reverseString(str) {
-    let x='';
-    for (let i = str.length-1; i >=0; i--) {
+// function reverseString(str) {
+//     let x='';
+//     for (let i = str.length-1; i >=0; i--) {
       
-       x+=str[i];
+//        x+=str[i];
        
+//     }
+//     return x;
+    
+// }
 
 
+// console.log(reverseString("hello"));
+
+
+// var str='FazLiDiN';
+var news='';
+
+function names(str) {
+    for (let i = 0; i < str.length; i++) {
+    
+        // console.log(str[i],str[i].toUpperCase());
+        if (str[i]==str[i].toUpperCase()) {
+          news+=str[i]
+          
+            
+        }
+    
+    
+        
     }
-    return x;
+    console.log(news);
     
 }
 
 
-console.log(reverseString("hello"));
+
+names('FaZliDdin');
