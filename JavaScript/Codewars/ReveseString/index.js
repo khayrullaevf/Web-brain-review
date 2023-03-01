7d3c0e76125beb8670c03405acbee917bcf439ef
fazliddin3779 <fazliddinkhayrullaev4@gmail.com>
2023-03-01 11:15:15 +0500

    ReverseString

---
+++ JavaScript/Codewars/ReveseString/index.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+function reverseString(str) {
+    let x='';
+    for (let i = str.length-1; i >=0; i--) {
+      
+       x+=str[i];
+       
+
+
+    }
+    return x;
+    
+}
+
+
+console.log(reverseString("hello"));
--- JavaScript/Lesson-6/while/index.js
+++ JavaScript/Lesson-6/while/index.js
@@ -1,19 +1,42 @@
 
+// 
+
+//  let str= 'goal';
+
+//  for (let i = str.length-1; i >=0; i--) {
+ 
+//     console.log(str[i]);
 
 
-//  let me='FazliddinKhayrullaev';
+//  }
 
-//  for (let i = 0; i < str.length; i++) {
-//    console.log(str[i]);
+// for (let i = 10; i >=0 ; i--) {
+//    console.log(i);
+    
+// }
+
+
+
+//  for (let i = 1; i <10; i++) {
+//     console.log('I love you');
     
 //  }
 
 
 
- for (let i = 1; i <10; i++) {
-    console.log('I love you');
+
+function reverseString(str) {
+    let x='';
+    for (let i = str.length-1; i >=0; i--) {
+      
+       x+=str[i];
+       
+
+
+    }
+    return x;
     
- }
+}
 
 
-
+console.log(reverseString("hello"));
