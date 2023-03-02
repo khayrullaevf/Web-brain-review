6b5a2aadeaf218c408fb8616cfee45418692de59
fazliddin3779 <fazliddinkhayrullaev4@gmail.com>
2023-03-02 10:27:13 +0500

    names array

--- JavaScript/Lesson-8/Arrays/index.js
+++ JavaScript/Lesson-8/Arrays/index.js
@@ -3,15 +3,15 @@
 
 // console.log('Meee');
 
-let me=['Fazliddin', 'Khayrullaev', 'HasanOgli'];
+// let me=['Fazliddin', 'Khayrullaev', 'HasanOgli'];
 
 
 
-var lastWord=me[me.length-1];
+// var lastWord=me[me.length-1];
 
 
-var lastLetter=lastWord[lastWord.length-1];
-console.log(lastLetter);
+// var lastLetter=lastWord[lastWord.length-1];
+// console.log(lastLetter);
 
 
 
@@ -28,11 +28,28 @@ console.log(lastLetter);
 
 
 
-let arr=[1,2,3,2,8,2,9,4,100,56] ; 
-console.log(arr.sort((a,b)=>b-a));  // teskari sort
+// let arr=[1,2,3,2,8,2,9,4,100,56] ; 
+// console.log(arr.sort((a,b)=>b-a));  // teskari sort
 
-console.log(arr.sort((a,b)=>a-b)); //sorting bigger numbers
+// console.log(arr.sort((a,b)=>a-b)); //sorting bigger numbers
 
 
-console.log(arr.join(''));
+// console.log(arr.join(''));
+
+
+let names=['fazliddin','raykona','shamsiddin','shahobiddin','hasan','zargul'];
+
+
+let firstName=names[0]
+// console.log(firstName);
+let lastName=names[names.length-1];
+console.log(lastName);  
+
+let lastWord=lastName[lastName.length-1];
+lastWord+='!'
+// console.log(lastWord); // returns !n
+
+let middleName=names[Math.floor(names.length/2)];
+// console.log(middleName);  //returns shahobddin
+
 
