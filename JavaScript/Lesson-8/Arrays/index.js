cfbbff6c3cdbaf9110ee68daf22de29f8c839cac
fazliddin3779 <fazliddinkhayrullaev4@gmail.com>
2023-03-02 10:03:28 +0500

    Array

---
+++ JavaScript/Lesson-8/Arrays/index.js
@@ -0,0 +1,36 @@
+
+
+
+// console.log('Meee');
+
+let me=['Fazliddin', 'Khayrullaev', 'HasanOgli'];
+
+
+
+var lastWord=me[me.length-1];
+
+
+var lastLetter=lastWord[lastWord.length-1];
+console.log(lastLetter);
+
+
+
+
+
+
+
+// Array=>massiv
+
+//sort , slice , splice, join , concat , push shift unshift
+
+
+
+
+
+
+let arr=[1,2,3,2,8,2,9,4,100,56] ; 
+console.log(arr.sort((a,b)=>b-a)); 
+
+
+
+
