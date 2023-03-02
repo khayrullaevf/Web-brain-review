5ca7b807bd2e60dbb74483dd014d606d270f28ac
fazliddin3779 <fazliddinkhayrullaev4@gmail.com>
2023-03-02 16:06:57 +0500

    Array-2

--- JavaScript/Lesson-8/Arrays/index.js
+++ JavaScript/Lesson-8/Arrays/index.js
@@ -43,7 +43,7 @@ let names=['fazliddin','raykona','shamsiddin','shahobiddin','hasan','zargul'];
 let firstName=names[0]
 // console.log(firstName);
 let lastName=names[names.length-1];
-console.log(lastName);  
+// console.log(lastName);  
 
 let lastWord=lastName[lastName.length-1];
 lastWord+='!'
@@ -52,4 +52,119 @@ lastWord+='!'
 let middleName=names[Math.floor(names.length/2)];
 // console.log(middleName);  //returns shahobddin
 
+let letters=['a','d','m','r','n','b','A','C'];
+// console.log(letters.sort((a,b)=>b.localeCompare(a)));
+// console.log(letters.sort((v,w)=>v.localeCompare(w)).join('**')); 
 
+
+let me='FazojidhhiHKJoiqd';
+// console.log(me.match(/[a-z]/g).join(''));
+
+// console.log(me.match(/[A-Z]/g).join(''));
+
+// console.log(letters.concat('FUCK YOU','Suck'));
+
+//concat qushib beradi;
+
+
+// const cities = ["Tokyo","Cairo","Los Angeles","Paris","Seattle"];
+// //   cities.splice(0,4);
+// cities.slice(0,4)
+
+//   console.log(cities);
+
+// const newCityArr = cities.slice(2); // agar index berilmasa nol dan hisoblaydi
+
+// console.log("Original: ", cities)
+// console.log("New: ", newCityArr)
+// console.log(letters.slice(2,4));
+// console.log(letters);
+// console.log(letters.splice(2,4));
+// console.log(letters);
+
+
+// var food = ['pizza', 'cake', 'salad', 'cookie'];
+// // console.log(food[food.length-1].split('').reverse().join(''));
+//   for (let i = 0; i < food.length; i++) {
+//    console.log( food[i] .split('').reverse().join(''));
+    
+//   }
+
+// // food.splice(1,0,"burrito")
+// // console.log(food);
+//  console.log(food.splice(1,2));
+
+// food.splice(0,2); //salad, cookie
+// food.splice(0,1)
+// console.log(food);
+
+// var str='Fazliddin'
+// console.log(str.split('').reverse().join('').toLowerCase());
+
+// let num=156238798;
+// let sortNum=+num.toString().split('').sort((a,b)=>a-b).join('');
+// // sortNum= +sortNum
+// console.log( `sorted num:  ${  sortNum} `);
+// console.log('original: ' +num);
+
+// console.log(typeof sortNum);
+
+
+
+let arr=['olma','gilos','behi','nok']
+// arr.push('behi','nok');
+// arr.pop()
+// arr.unshift('banana')
+// arr.concat('me') 
+// console.log(arr.concat('me', 'you' ,'we'));
+// arr.splice(1,2 , 'we');
+// console.log(arr);
+
+// let newarr= arr.forEach((value,index,array)=>{
+// // console.log('array :' +index);
+// // console.log( value);
+// // console.log( index);
+// // console.log( array);
+// return 'hello'
+
+
+
+// });
+
+// arr.forEach(element => {
+//      console.log(element);
+// });
+// console.log(newarr);
+ //arr[i]=value;
+ //index=i;
+ //arr=array
+
+for (let i = 0; i < arr.length; i++) {
+    // console.log(arr); //===array; =['olma','gilos','behi','nok']
+//                                    ['olma','gilos','behi','nok']
+//                                    ['olma','gilos','behi','nok']
+//                                    ['olma','gilos','behi','nok']
+
+// console.log('i love you');
+
+
+    // console.log(arr[i]); //===value; olma , gilos , behi , nok
+    // console.log(i); //===index;  0, 1, 2, 3
+    
+}
+
+
+
+// let nums=[12,4,4,55,77,45,65,34,6,64,70]
+
+
+// let newNum=nums.find((value ,index,array )=>{
+
+//     // console.log(array);
+//     return value!==51;
+// }
+
+        
+// )
+
+// console.log(newNum);
---
+++ JavaScript/Lesson-9/index.js
@@ -0,0 +1,52 @@
+
+
+
+
+
+
+// let str='AABBCCC';
+
+
+// let count=0;
+
+// for (let i = 0; i < str.length; i++) {
+//     //    console.log(str[i]);
+//     if (str[i]!==str[i]) {
+//         count+=str[i]
+//         // console.log(count);
+//     }
+   
+// }
+
+
+// let newArr=str.split('');
+// let count=0;
+//  let sortedArray=newArr.filter((value,index,array)=>{
+   
+
+
+
+//  })
+
+//  console.log(sortedArray);
+
+
+//value=arr[i]
+//index=i;
+//array=arr
+
+
+// let arr =[1,5,2,3,4,8];
+
+
+//  arr.map((value,index,array)=>{
+
+//     console.log('map :' +array);
+//  })
+
+// for (let i = 0; i < arr.length; i++) {
+//    console.log( 'for :' +arr);
+// }
+
+
+
