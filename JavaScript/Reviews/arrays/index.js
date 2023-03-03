34a9e15a58ce349e4b575ce4e93ae3cdb4eb8f25
fazliddin3779 <fazliddinkhayrullaev4@gmail.com>
2023-03-03 12:15:45 +0500

    Array-2

--- JavaScript/Reviews/arrays/index.js
+++ JavaScript/Reviews/arrays/index.js
@@ -21,9 +21,60 @@ lastletter+='**'
 
  let numbers=[9,2,5,1,45,76,1222,465,7798,3,8,7,4,6];
 //  console.log(numbers.sort((x,y)=>y-x)); teskari sort
+//  console.log(numbers.reduce((a,b)=>a+ +b));
+
 
 let str=['j','a','p','A','L','b','V' ,'o','E','c'];
+let str2=['me']
 
+// console.log(str.slice(2,7)); //'p','A','L','b','V' 2nchi indexdan boshlab  7nchi lenghtgacha copy qiladi
+// console.log(str);  //['j','a','p','A','L','b','V' ,'o','E','c'];
+
+// console.log(str.splice(2,7)); //'p','A','L','b','V' ,'o','E' 2nchi indexdan 7 ta length ; ,kesib beradi
+// console.log(str); //'p','A','L','b','V' ,'o','E'  qirqib tashlaydi
+// str.splice(1,9 ,'Men');
+// console.log(str);
+
+
+
+let students=['Fazliddin','Rayhona','Shamsiddin'];
+//  students.push("SEN",'MEN');  oxiridan 2ta item qushib beradi;
+//  students.pop() //oxiridan  1ta item olib tashlaydi
+//  students.pop() // oxiridan  1ta item olib tashlaydi
+//  students.pop() //oxiridan  1ta item olib tashlaydi
+//  console.log(students);
+// for (let i = 0; i < students.length; i++) {
+//     // console.log(students[i]);
+//     for (let j = students.length; j >=0; j--) {
+       
+//         console.log(j);
+        
+    
+       
+//         // console.log(students[j]);
+        
+//     }
+    
+     
+// }
+
+// let m='WBA';
+// // console.log(typeof m);
+
+
+
+
+// for (let i = m.length-1; i >=0; i--) {
+// //    console.log(m[i]);
+    
+// }
+
+
+
+
+// console.log(str.concat(str2));
 // console.log(str.sort((x,y)=>x.localeCompare(y))); sort qilish katta harflarni ham
+
+// console.log( typeof str.join('')); //arrayni stringga utkaizib beradi string qaytaradi
 
 
