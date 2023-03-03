19798f309df72159dbcffe5da853722c531c2a40
fazliddin3779 <fazliddinkhayrullaev4@gmail.com>
2023-03-03 16:07:44 +0500

    Keys

---
+++ JavaScript/Reviews/obejcts/index.js
@@ -0,0 +1,20 @@
+
+
+
+ let me={
+    name:'Fazliddin',
+ }
+
+
+ console.log(me.name);
+ console.log(me.surname='Khayrullaev');
+ console.log(me);
+
+ console.log(me['name']='EMINEM');
+//  console.log(Object.keys(me));
+ console.log(Object.keys(me).findIndex(v=>v=='name'));
+//  console.log(Object.values(me));
+//  console.log(Object.values(me));
+ console.log(Object.values(me));
+
+
