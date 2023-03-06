cc3a0852e538ac647186407af7d76d136d39e7a3
fazliddin3779 <fazliddinkhayrullaev4@gmail.com>
2023-03-06 16:48:59 +0500

    Non-primitive

---
+++ JavaScript/Lesson-11/Reviews/objects/index.js
@@ -0,0 +1,33 @@
+
+
+
+// let nums=[32,21,45,14,78,65,45]
+
+// let nums2=nums.filter(evenOdd);
+// function evenOdd(x) {
+//  return x%3==0||x%5==0
+// }
+
+// console.log(nums2);
+
+// var str='64437';
+// str=str.toString().split('')
+
+
+// console.log(str.reduce((a,b)=>a+ +b,0));
+
+// var total=0;
+
+// for (let i = 0; i < str.length; i++) {
+//    total+= +str[i]
+    
+// }
+
+// console.log(total);
+
+
+var arr=['Messi','Ronaldo']
+
+var[Goat , Trash]=arr;
+// console.log(Goat);
+// console.log(Trash);
