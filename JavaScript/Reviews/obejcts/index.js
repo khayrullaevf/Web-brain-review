85dde2c0821b9538d183f24e1e3ea0a5bb398107
fazliddin3779 <fazliddinkhayrullaev4@gmail.com>
2023-03-06 21:30:34 +0500

    Call,apply , bind

--- JavaScript/Reviews/obejcts/index.js
+++ JavaScript/Reviews/obejcts/index.js
@@ -142,48 +142,111 @@
 // //  console.log(Object.values(me));
 // //  console.log(Object.values(me));
 
-//  let a =Object.entries(me); //qiymat va key ni array ichida array qilib olib beradi
-//  console.log(a);
+// //  let a =Object.entries(me); //qiymat va key ni array ichida array qilib olib beradi
+// //  console.log(a);
 
-//  console.log(Object.fromEntries(a));
-
-
+// //  console.log(Object.fromEntries(a));
 
 
 
 
-// var str='AVCCAVBDBACC';
-// let arr=[];
 
 
-// for (let i = 0; i < str.length; i++) {
+// // var str='AVCCAVBDBACC';
+// // let arr=[];
+
+
+// // for (let i = 0; i < str.length; i++) {
  
 
-//     if (arr.includes(str[i])) {
-//         console.log(str[i]); break
+// //     if (arr.includes(str[i])) {
+// //         console.log(str[i]); break
         
-//     }
+// //     }
 
-//     arr.push(str[i])
+// //     arr.push(str[i])
 
  
     
+// // }
+
+
+
+// // for (let i = 0; i < 10; i++) {
+// //      if (i===3) {
+// //         break
+// //      }
+// //      console.log(i);
+// // }
+
+
+// var arr =[2,3,[3,4,[3,4]]] //19
+// var str=['Fazliddin',['Shamsiddin',['Shahobiddin',['Rayhona']]]]
+
+// // console.log(arr.flat(Infinity).reduce((a,b)=>a+ +b));  //nested arrayni bitta arrayga utkazish uchun arr.flat(Infinity) ishlatiladi;
+// // console.log(str.flat(Infinity));
+
+
+
+
+
+// var str3='ABDABACC';
+
+
+//  var emptArr=[];
+// for (let i = 0; i < str3.length; i++) {
+//        if (emptArr.includes(str3[i])) {
+//       console.log(str3[i]); break
+        
+//        }
+
+  
+
+//      emptArr.push(str3[i])
 // }
 
 
 
-// for (let i = 0; i < 10; i++) {
-//      if (i===3) {
-//         break
-//      }
-//      console.log(i);
-// }
+
+// var nestedArr=[2,3,4,[3,4,[6,3,[2,4]]]]; ///31
+var arr=[2,5,3,4];
+// console.log(arr.map(v=>v**2));
+// console.log(arr.map(v=>[v**2]).flat(Infinity));
+// console.log(arr.map(v=>[v**2]).flat(Infinity));
+// console.log(arr.flatMap(v=>[v**2])); // map and flat both in flatMap();
+ let newArr=[56,65]
+ let obj={
+    id:1,
+    name:'Messi',
+    status:'GOAT',
+    addNew:function (age) {
+
+      
+      console.log(this.pro+'!  your age is '+this.year +'   your  age is '+age);
+    }
+ }
+
+ let newObj={
+   pro:'The king',
+   year: 1985,
+ }
 
 
-var arr =[2,3,[3,4,[3,4]]] //19
-var str=['Fazliddin',['Shamsiddin',['Shahobiddin',['Rayhona']]]]
+//  let callingBind=obj.addNew.bind(newObj);
+//  callingBind(20)
+//  callingBind(48)
 
-// console.log(arr.flat(Infinity).reduce((a,b)=>a+ +b));  //nested arrayni bitta arrayga utkazish uchun arr.flat(Infinity) ishlatiladi;
-// console.log(str.flat(Infinity));
+obj.addNew.bind(newObj)(34)
+ 
 
 
+//  obj.addNew.call(newObj, 35);
+//  obj.addNew.apply(newObj, [35]);
+ //call va apply ning farqi  shundaki apply da  argument berishda  arraydan foydalanamiz
+
+//  console.log({...obj , age:35, country:'Argentina'});
+//  console.log([...newArr, ...arr]);
+
+// console.log(nestedArr.flat(Infinity)); //  bitta arrayga solib beradi;
+
+// console.log(nestedArr.flat(Infinity).reduce((a,b)=>a+ +b));
