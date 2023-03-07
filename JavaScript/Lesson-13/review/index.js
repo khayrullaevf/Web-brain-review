5b3aad4a98b978eb21b458f9a00786d734155f5b
fazliddin3779 <fazliddinkhayrullaev4@gmail.com>
2023-03-07 14:32:57 +0500

    SetTimeout

--- JavaScript/Lesson-13/review/index.js
+++ JavaScript/Lesson-13/review/index.js
@@ -2,3 +2,50 @@
 
 
 
+let a =1;
+let b=2;
+let c=3;
+
+
+let item=document.getElementById('clear')
+// console.log(item);
+
+ const time=setTimeout(() => {
+
+    item.innerHTML='Downloaded'
+   
+        
+     }, 3000);
+
+     const clearer=()=>{
+        clearInterval(time)
+    }
+    
+
+
+    
+//  setTimeout(() => {
+// console.log(c);
+    
+//  }, 5000);
+//  setTimeout(() => {
+//     console.log(c+1);
+        
+//      }, 7000);
+ 
+
+//  const time=setInterval(() => {
+//     console.log(a++);
+    
+//  }, 400);
+// //  clearInterval((item))/
+
+
+
+//  fetch('/').then((res)=>{
+//     console.log("fetched");
+//  }) 
+
+
+// console.log(a);
+// console.log(b);
