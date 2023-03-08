533404cf55a1fbe60dc1cffe4b6529fee70f53b3
fazliddin3779 <fazliddinkhayrullaev4@gmail.com>
2023-03-08 12:13:11 +0500

    DOM-1

---
+++ JavaScript/Lesson-13/CallBackHell/index.js
@@ -0,0 +1,44 @@
+
+
+// console.log('Hello');
+
+// setTimeout(function()  {
+//     console.log('JavaScript');
+    
+// }, 5000);
+
+
+
+
+
+// console.log('Season 2');
+
+const card=['shoes','pants','kurta']
+
+ createOrder(card ) 
+
+
+   const promise= createOrder(card)
+
+   promise.then(function(orderId){
+    proceedToPayment(orderId);
+   
+    })
+
+
+
+ 
+
+
+
+
+
+
+// api.createOrder(card, function (){
+    
+    
+//     api.proceedToPaymet()
+    
+//      })
+
+
