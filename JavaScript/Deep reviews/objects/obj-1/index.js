9b14b5da6ffc750fc63be9d8f470aa334781dc09
fazliddin3779 <fazliddinkhayrullaev4@gmail.com>
2023-03-13 15:08:00 +0500

    Map Objects

--- JavaScript/Deep reviews/objects/obj-1/index.js
+++ JavaScript/Deep reviews/objects/obj-1/index.js
@@ -23,6 +23,21 @@ const me2={
 }
 const {dad,age}=me2
 // me.hiMe.call(me2);
-console.log(name);
-console.log(dad);
-console.log(age);
+// console.log(name);
+// console.log(dad);
+// console.log(age);
+
+
+
+const people=[
+    {id:1,name:"Fazliddin",surname:'Khayrullaev', status:'IT'},
+    {id:2,name:"Dani",surname:'Alves', status:'Most Trophy'},
+    {id:3,name:"Gerard",surname:'Pique', status:'Mr President'},
+    {id:4,name:"Ronald",surname:'Araujo', status:'Gorilla'},
+]
+
+
+const players=people.map(({id,name,surname,status})=>{
+
+    console.log(`${id}) ${name} ${surname} , your status is ${status}`);
+})
