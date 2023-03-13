44d13dce4eda7068b2fe48478e12c7bc97549583
khayrullaevf <fazliddinkhayrullaev4@gmail.com>
2023-03-13 19:55:21 +0500

    Class-1

--- JavaScript/Deep reviews/classes/index.js
+++ JavaScript/Deep reviews/classes/index.js
@@ -7,13 +7,22 @@ let str='23hg221h9jh1';
 
 // console.log(str.match(/[0-9]/g).reduce((a,b)=>a+ +b,0));
 
-let total=0;
-for (let i = 0; i < str.length; i++) {
-   if (+str[i]==str[i]) {
-    //   total+=+str[i]
-      total+=parseInt(str[i])
-   }
+// let total=0;
+// for (let i = 0; i < str.length; i++) {
+//    if (+str[i]==str[i]) {
+//     //   total+=+str[i]
+//       total+=parseInt(str[i])
+//    }
     
+// }
+
+// console.log(total);
+
+
+function User(name){
+    this.name=name;
 }
 
-console.log(total);
+let user=new User('Abror')
+
+console.log(user.name);
--- React/first_app/src/myapp.jsx
+++ React/first_app/src/myapp.jsx
@@ -2,13 +2,22 @@
 import './myapp.css';
 
 
+function onChange(){
+  
+
+
+}
+
+
+
 function Me() {
 
 return(
 
 
     <div>
-        <h1>Hi Fazliddin</h1>
+        <h1 >Hi Fazliddin</h1>
+        <h2 className="title" onChange={onChange()}>This is a portfolio Website</h2>
     </div>
 )
 
